b7d5e36bdd2d35d082bab8e83aca0bce98893225
manzanareslopezdionicio <118032880+manzanareslopezdionicio@users.noreply.github.com>
2025-05-26 16:40:36 -0600

    tiempo

--- static/js/scripts.js
+++ static/js/scripts.js
@@ -16,7 +16,7 @@ document.addEventListener('DOMContentLoaded', function() {
             toast: true,
             position: 'top-end',
             showConfirmButton: false,
-            timer: 3000,
+            timer: 2000,
             timerProgressBar: true
         });
     }
