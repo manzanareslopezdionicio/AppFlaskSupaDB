516436403d9dfa5e86552350cde9f63af51111d3
manzanareslopezdionicio <118032880+manzanareslopezdionicio@users.noreply.github.com>
2025-05-25 22:28:51 -0600

    login

---
+++ static/js/estilonivel.js
@@ -0,0 +1,126 @@
+//mostrar los datos niveles y descripcion - conceptual
+const concepto = document.getElementById('concepto');
+const logro1 = document.getElementById('logro1');
+const labellogro = document.getElementById('labellogro');
+const nivel5 = document.getElementById('nivel5');
+const label5 = document.getElementById('label5');
+const nivel4 = document.getElementById('nivel4');
+const label4 = document.getElementById('label4');
+const nivel3 = document.getElementById('nivel3');
+const label3 = document.getElementById('label3');
+const nivel2 = document.getElementById('nivel2');
+const label2 = document.getElementById('label2');
+const leyenda1 = document.getElementById('leyenda1');
+
+concepto.addEventListener('change', function () {
+    if (concepto.checked) {
+        logro1.style.display = 'block'; // O 'inline' si se necesita
+        labellogro.style.display = 'block';
+        nivel5.style.display = 'block';
+        label5.style.display = 'block';
+        nivel4.style.display = 'block';
+        label4.style.display = 'block';
+        nivel3.style.display = 'block';
+        label3.style.display = 'block';
+        nivel2.style.display = 'block';
+        label2.style.display = 'block';
+        leyenda1.style.display = 'block';
+    } else {
+        logro1.style.display = 'none';
+        labellogro.style.display = 'none';
+        nivel5.style.display = 'none';
+        label5.style.display = 'none';
+        nivel4.style.display = 'none';
+        label4.style.display = 'none';
+        nivel3.style.display = 'none';
+        label3.style.display = 'none';
+        nivel2.style.display = 'none';
+        label2.style.display = 'none';
+        leyenda1.style.display = 'none';
+    }
+});
+
+//mostrar los datos niveles y descripcion - procedimental
+const proceso = document.getElementById('proceso');
+const logro2 = document.getElementById('logro2');
+const labellogro2 = document.getElementById('labellogro2');
+const nivel55 = document.getElementById('nivel55');
+const label55 = document.getElementById('label55');
+const nivel44 = document.getElementById('nivel44');
+const label44 = document.getElementById('label44');
+const nivel33 = document.getElementById('nivel33');
+const label33 = document.getElementById('label33');
+const nivel22 = document.getElementById('nivel22');
+const label22 = document.getElementById('label22');
+const leyenda2 = document.getElementById('leyenda2');
+
+proceso.addEventListener('change', function () {
+    if (proceso.checked) {
+        logro2.style.display = 'block'; // O 'inline' si se necesita
+        labellogro2.style.display = 'block';
+        nivel55.style.display = 'block';
+        label55.style.display = 'block';
+        nivel44.style.display = 'block';
+        label44.style.display = 'block';
+        nivel33.style.display = 'block';
+        label33.style.display = 'block';
+        nivel22.style.display = 'block';
+        label22.style.display = 'block';
+        leyenda12style.display = 'block';
+    } else {
+        logro2.style.display = 'none';
+        labellogro2.style.display = 'none';
+        nivel55.style.display = 'none';
+        label55.style.display = 'none';
+        nivel44.style.display = 'none';
+        label44.style.display = 'none';
+        nivel33.style.display = 'none';
+        label33.style.display = 'none';
+        nivel22.style.display = 'none';
+        label22.style.display = 'none';
+        leyenda2.style.display = 'none';
+    }
+});
+
+//mostrar los datos niveles y descripcion - Actitudinal
+const actitud = document.getElementById('actitud');
+const logro3 = document.getElementById('logro3');
+const labellogro3 = document.getElementById('labellogro3');
+const nivel555 = document.getElementById('nivel555');
+const label555 = document.getElementById('label555');
+const nivel444 = document.getElementById('nivel444');
+const label444 = document.getElementById('label444');
+const nivel333 = document.getElementById('nivel333');
+const label333 = document.getElementById('label333');
+const nivel222 = document.getElementById('nivel222');
+const label222 = document.getElementById('label222');
+const leyenda3 = document.getElementById('leyenda3');
+
+actitud.addEventListener('change', function () {
+    if (actitud.checked) {
+        logro3.style.display = 'block'; // O 'inline' si se necesita
+        labellogro3.style.display = 'block';
+        nivel555.style.display = 'block';
+        label555.style.display = 'block';
+        nivel444.style.display = 'block';
+        label444.style.display = 'block';
+        nivel333.style.display = 'block';
+        label333.style.display = 'block';
+        nivel222.style.display = 'block';
+        label222.style.display = 'block';
+        leyenda3.style.display = 'block';
+    } else {
+        logro3.style.display = 'none';
+        labellogro3.style.display = 'none';
+        nivel555.style.display = 'none';
+        label555.style.display = 'none';
+        nivel444.style.display = 'none';
+        label444.style.display = 'none';
+        nivel333.style.display = 'none';
+        label333.style.display = 'none';
+        nivel222.style.display = 'none';
+        label222.style.display = 'none';
+        leyenda3.style.display = 'none';
+    }
+});
+
